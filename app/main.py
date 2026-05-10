9fbf629e4d03de933302585b76b90320a2f5350f
CharliesCas <charlieofficial.hernan@gmail.com>
2026-05-10 02:59:12 -0600

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI
 from app.core.database import Base,engine
 from app.games.router import games
 from app.genres.router import genres
+from fastapi.middleware.cors import CORSMiddleware
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(games)
 app.include_router(genres)
-
-
-
-
